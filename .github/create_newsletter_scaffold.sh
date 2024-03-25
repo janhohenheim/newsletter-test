80e3559555a9aaeea63ee8900eadf322960550da
Jan Hohenheim <jan@hohenheim.ch>
2024-03-25 22:09:27 +0100

    Fix syntax

--- .github/create_newsletter_scaffold.sh
+++ .github/create_newsletter_scaffold.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 if [[ -z "${NEWSLETTER_MONTH}" ]]; then
   echo "NEWSLETTER_MONTH is not set. Exiting..."
@@ -23,12 +23,12 @@ if [[ -z "${NEWSLETTER_COUNTER}" ]]; then
 fi
 
 cp newsletter-template.md index.md
-sed -i "s/{{\s*NEWSLETTER_MONTH\s*}}/${{ NEWSLETTER_MONTH }}/g" index.md
-sed -i "s/{{\s*NEWSLETTER_YEAR\s*}}/${{ NEWSLETTER_YEAR }}/g" index.md
-sed -i "s/{{\s*NEWSLETTER_DEADLINE\s*}}/${{ NEWSLETTER_DEADLINE }}/g" index.md
-sed -i "s/{{\s*NEWSLETTER_COUNTER\s*}}/${{ NEWSLETTER_COUNTER }}/g" index.md
+sed -i "s/{{\s*NEWSLETTER_MONTH\s*}}/${NEWSLETTER_MONTH}/g" index.md
+sed -i "s/{{\s*NEWSLETTER_YEAR\s*}}/${NEWSLETTER_YEAR}/g" index.md
+sed -i "s/{{\s*NEWSLETTER_DEADLINE\s*}}/${NEWSLETTER_DEADLINE}/g" index.md
+sed -i "s/{{\s*NEWSLETTER_COUNTER\s*}}/${NEWSLETTER_COUNTER}/g" index.md
 # Create a dir in content/news with the counter with 3 digits as name
-destination="content/news/$(printf "%03d" ${{ NEWSLETTER_COUNTER }})"
+destination="content/news/$(printf "%03d" ${NEWSLETTER_COUNTER})"
 mkdir -p $destination
 mv index.md $destination/index.md
 git status
